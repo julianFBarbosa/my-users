2b867d658aee279732d0b5d661e31e0339eb95f9
Julian Barbosa <bjvlixn@gmail.com>
2022-01-06 20:49:27 -0300

    start to implement firebase

---
+++ utils/firebaseFunctions.js
@@ -0,0 +1,21 @@
+import firebase from "firebase";
+
+firebase
+const registerUser = (email, password) => {
+  firebase
+    .auth()
+    .createUserWithEmailAndPassword(email, password)
+    .catch(function (error) {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      if (errorCode == 'auth/weak-password') {
+        alert('The password is too weak.');
+      } else {
+        alert(errorMessage);
+      }
+      console.log(error);
+    });
+};
+
+export { registerUser };
